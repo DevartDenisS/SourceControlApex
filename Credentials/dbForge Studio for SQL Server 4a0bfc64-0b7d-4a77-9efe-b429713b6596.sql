b8840095f6c2ad746e06c1a16022de891b7e0977
DevartDenisS <DevartDenisS>
2025-04-28 19:45:50 +0300

    2

---
+++ Credentials/dbForge Studio for SQL Server 4a0bfc64-0b7d-4a77-9efe-b429713b6596.sql
@@ -0,0 +1,2 @@
+CREATE CREDENTIAL [dbForge Studio for SQL Server 4a0bfc64-0b7d-4a77-9efe-b429713b6596] WITH IDENTITY = 'deniss-home'
+GO
---
+++ Credentials/dbForge Studio for SQL Server ed630d63-2deb-4c6c-bd2b-f1ad98c8db6a.sql
@@ -0,0 +1,2 @@
+CREATE CREDENTIAL [dbForge Studio for SQL Server ed630d63-2deb-4c6c-bd2b-f1ad98c8db6a] WITH IDENTITY = 'sa'
+GO
